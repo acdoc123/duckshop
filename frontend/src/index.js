7748eacea921ab3eb1ba5a598cab7d8b4ccc77f6
acdoc123 <acdoc123@gmail.com>
2022-03-21 10:56:58 +0700

    make it better

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
